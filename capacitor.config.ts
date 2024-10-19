f0f0e0d2437a4ca9ddefebf5fa87acdb96e98583
Carolina Herrera Santana <carolhersant4@gmail.com>
2024-10-19 15:17:31 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DobleElectricidadApp',
+  webDir: 'www'
+};
+
+export default config;
